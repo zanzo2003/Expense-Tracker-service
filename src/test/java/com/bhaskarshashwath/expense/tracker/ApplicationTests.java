e001156d0c0ff9f243c4244d082a5f1de3e36b8c
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-05-16 19:37:25 +0530

    inital service application setup

---
+++ src/test/java/com/bhaskarshashwath/expense/tracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskarshashwath.expense.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
