988ce324812fc2e692cdeb263bd981004ec3a61d
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-05-16 22:13:08 +0530

    Custom User Details DTO

---
+++ src/main/java/com/bhaskarshashwath/expense/tracker/dto/CustomUserDetailsDTO.java
@@ -0,0 +1,42 @@
+package com.bhaskarshashwath.expense.tracker.dto;
+
+import com.bhaskarshashwath.expense.tracker.entities.UserInfo;
+import com.bhaskarshashwath.expense.tracker.entities.UserRoles;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetailsDTO extends UserInfo implements UserDetails {
+    private String username;
+    private String password;
+    Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetailsDTO(UserInfo user){
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        for(UserRoles authority: user.getRoles()){
+            authorityList.add(new SimpleGrantedAuthority(authority.getName().toUpperCase()));
+        }
+        this.authorities = authorityList;
+    }
+
+    @Override
+    public String getPassword(){
+        return this.password;
+    }
+
+    @Override
+    public String getUsername(){
+        return this.username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return this.authorities;
+    }
+}
