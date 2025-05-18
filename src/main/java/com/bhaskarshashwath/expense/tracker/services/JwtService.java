9d66a3c8dcde5f930ad460d6d4d9ea12b96ee169
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-05-18 20:13:57 +0530

    jwt service

---
+++ src/main/java/com/bhaskarshashwath/expense/tracker/services/JwtService.java
@@ -0,0 +1,56 @@
+package com.bhaskarshashwath.expense.tracker.services;
+
+import com.bhaskarshashwath.expense.tracker.entities.UserInfo;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+
+@Service
+public class JwtService {
+
+
+    private final String SECRET = "e8b9c4f2a1d7e5b0394c6a8f3d2e9b1c5a7f4d8e0b3c9a2f6e1d5b7a4c8f0e2";
+    private final int rounds = 12;
+
+    public String getUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        final Claims claims = extracatAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public Boolean validateToken(String token, UserInfo user){
+        return ( getUsername(token).equals(user.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Claims extracatAllClaims(String token){
+        return Jwts
+                .parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(this.SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
